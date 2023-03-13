cf5b0afa7a0c31cd210dffb2457bff73d27df0c6
Julian Pollinger <julian@pollinger.dev>
2023-03-13 12:06:26 +0100

    improve performance by using array for assignment numbers

--- src/algorithm.c
+++ src/algorithm.c
@@ -48,16 +48,10 @@ static int compareStudentsByPoints(const student *s1, const student *s2){
     return s2->mimiPoints-s1->mimiPoints;
 }
 
-static gboolean tryAssignment(student *s, seminar sel, GHashTable *assigments, int points, char semType){
-    int assignedStudents;
-    gboolean success = g_hash_table_lookup_extended(assigments, sel.id, NULL, (gpointer *) &assignedStudents);
-    //int *assignedStudentsPointer = g_hash_table_lookup(assigments, sel.id);
-    if (!success) {
-        dieWithoutErrno("Could not get number of assigned students");
-    }
+static gboolean tryAssignment(student *s, seminar sel, int *assigments, int points, char semType){
+    int assignedStudents = assigments[*sel.id];
     if (assignedStudents < sel.size) {
-        int newCount = assignedStudents + 1;
-        g_hash_table_replace(assigments, sel.id, GINT_TO_POINTER(newCount));
+        assigments[*sel.id] = assignedStudents + 1;
         s->mimiPoints += points;
         switch (semType) {
             case 'w':
@@ -94,24 +88,16 @@ GArray *batchRunAlgorithmn(int times,const GArray *students, const GArray *w_sem
 }
 
 GArray *runAlgorithm(const GArray *students, const GArray *w_seminars, const GArray *p_seminars) {
-    GHashTable *table = g_hash_table_new_full(g_int_hash, g_int_equal, NULL, NULL);
-    for (int i = 0; i < w_seminars->len; i++) {
-        seminar s = g_array_index(w_seminars, seminar, i);
-        g_hash_table_insert(table, s.id, GINT_TO_POINTER(0));
-    }
-    for (int i = 0; i < p_seminars->len; i++) {
-        seminar s = g_array_index(p_seminars, seminar, i);
-        g_hash_table_insert(table, s.id, GINT_TO_POINTER(0));
-    }
+    int *assignments = calloc(w_seminars->len + p_seminars->len, sizeof(int));
 
     GArray *cStudents = copyStudents(students);
     int *intrange = getIntRange(cStudents->len);
     shuffle(intrange, cStudents->len);
     for (int i = 0; i < cStudents->len; i++) {
         student *s = &g_array_index(cStudents, student, intrange[i]);
-        if (!tryAssignment(s, s->wVotes[0], table, default_points.first_selection, 'w')) {
-            if (!tryAssignment(s, s->wVotes[1], table, default_points.second_selection, 'w')) {
-                if (!tryAssignment(s, s->wVotes[2], table, default_points.third_selection, 'w')) {
+        if (!tryAssignment(s, s->wVotes[0], assignments, default_points.first_selection, 'w')) {
+            if (!tryAssignment(s, s->wVotes[1], assignments, default_points.second_selection, 'w')) {
+                if (!tryAssignment(s, s->wVotes[2], assignments, default_points.third_selection, 'w')) {
                     s->mimiPoints += default_points.no_selection;
                 }
             }
@@ -122,15 +108,15 @@ GArray *runAlgorithm(const GArray *students, const GArray *w_seminars, const GAr
     g_array_sort(cStudents, (GCompareFunc) compareStudentsByPoints);
     for (int i = 0; i < cStudents->len; i++) {
         student *s = &g_array_index(cStudents, student, i);
-        if (!tryAssignment(s, s->pVotes[0], table, default_points.first_selection, 'p')) {
-            if (!tryAssignment(s, s->pVotes[1], table, default_points.second_selection, 'p')) {
-                if (!tryAssignment(s, s->pVotes[2], table, default_points.third_selection, 'p')) {
+        if (!tryAssignment(s, s->pVotes[0], assignments, default_points.first_selection, 'p')) {
+            if (!tryAssignment(s, s->pVotes[1], assignments, default_points.second_selection, 'p')) {
+                if (!tryAssignment(s, s->pVotes[2], assignments, default_points.third_selection, 'p')) {
                     s->mimiPoints += default_points.no_selection;
                 }
             }
         }
 
     }
-    g_hash_table_destroy(table);
+    free(assignments);
     return cStudents;
 }
